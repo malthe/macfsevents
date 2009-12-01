c00e65ce249c7747d433e188a2a10461a6f95e70
Malthe Borch <mborch@gmail.com>
2009-12-01 16:25:24 +0100

    Improved test coverage for multiple files and subdirectories.

--- tests.py
+++ tests.py
@@ -2,13 +2,7 @@ import unittest
 
 class PathObservationTestCase(unittest.TestCase):
     def setUp(self):
-        import os
-        import tempfile
-        tempdir = tempfile.gettempdir()
-        f = tempfile.NamedTemporaryFile(dir=tempdir)
-        self.tempdir = os.path.join(tempdir, os.path.basename(f.name))
-        f.close()
-        os.mkdir(self.tempdir)
+        self.tempdir = self._make_tempdir()
 
     def tearDown(self):
         import os
@@ -22,6 +16,16 @@ class PathObservationTestCase(unittest.TestCase):
         f = tempfile.NamedTemporaryFile(dir=directory)
         path = os.path.realpath(os.path.dirname(f.name)).rstrip('/') + '/'
         return f, path
+
+    def _make_tempdir(self):
+        import os
+        import tempfile
+        tempdir = tempfile.gettempdir()
+        f = tempfile.NamedTemporaryFile(dir=tempdir)
+        tempdir = os.path.join(tempdir, os.path.basename(f.name))
+        f.close()
+        os.mkdir(tempdir)
+        return tempdir
 
     def test_single_file_added(self):
         events = []
@@ -52,6 +56,53 @@ class PathObservationTestCase(unittest.TestCase):
 
         f.close()
         self.assertEquals(events, [(path, 0)])
+
+    def test_multiple_files_added(self):
+        events = []
+        def callback(*args):
+            events.append(args)
+
+        from fsevents import Observer
+        observer = Observer()
+        from fsevents import Stream
+        observer.start()
+
+        # wait until activation
+        import time
+        while not observer.isAlive():
+            time.sleep(0.1)
+        time.sleep(0.1)
+
+        # two files in same directory
+        import os
+        path1 = os.path.realpath(self._make_tempdir()) + '/'
+        f = self._make_temporary(path1)[0]
+        g = self._make_temporary(path1)[0]
+
+        # one file in a separate directory
+        path2 = os.path.realpath(self._make_tempdir()) + '/'
+        h = self._make_temporary(path2)[0]
+
+        stream = Stream(callback, path1, path2)
+        observer.schedule(stream)
+
+        try:
+            f.flush()
+            g.flush()
+            h.flush()
+            time.sleep(0.1)
+            self.assertEqual(sorted(events), sorted([(path1, 0), (path2, 0)]))
+        finally:
+            f.close()
+            g.close()
+            h.close()
+            os.rmdir(path1)
+            os.rmdir(path2)
+
+            # stop and join observer
+            observer.stop()
+            observer.unschedule(stream)
+            observer.join()
 
     def test_single_file_added_multiple_streams(self):
         events = []
@@ -154,19 +205,16 @@ class PathObservationTestCase(unittest.TestCase):
             events.append(args)
 
         import os
-        tempdir = self.tempdir
-        directory = os.path.join(tempdir, "fsevents")
-        directory = os.path.realpath(directory).rstrip('/') + '/'
-        if os.path.exists(directory):
-            removedir = False
-        else:
-            os.mkdir(directory)
-            removedir = True
+        directory = self._make_tempdir()
+        subdirectory = os.path.realpath(os.path.join(directory, 'subdir')) + '/'
+        os.mkdir(subdirectory)
+        import time
+        time.sleep(1)
+        f, path = self._make_temporary(subdirectory)
 
         try:
-            f, path = self._make_temporary(directory)
             from fsevents import Stream
-            stream = Stream(callback, path)
+            stream = Stream(callback, directory)
 
             from fsevents import Observer
             observer = Observer()
@@ -185,11 +233,11 @@ class PathObservationTestCase(unittest.TestCase):
             observer.unschedule(stream)
             observer.join()
 
-            self.assertEquals(events, [(directory, 0)])
+            self.assertEquals(events, [(subdirectory, 0)])
         finally:
             f.close()
-            if removedir:
-                os.rmdir(directory)
+            os.rmdir(subdirectory)
+            os.rmdir(directory)
 
     def test_single_file_added_unschedule_then_stop(self):
         events = []
