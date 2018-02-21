d0f449dd9b183d30053d5d5bc4d7e6acc0be211b
Malthe Borch <mborch@gmail.com>
2018-02-21 12:41:32 +0100

    Prepare new development

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ ext_modules = [
     ]
 
 setup(name = "MacFSEvents",
-      version = "0.8.1",
+      version = "0.9-dev",
       description = "Thread-based interface to file system observation primitives.",
       long_description = "\n\n".join((read('README.rst'), read('CHANGES.rst'))),
       license = "BSD",
